8a243dff1069e662e176403b1074cbdde70f177a
Barthelemy-BENGLOAN <bartbengloan@icloud.com>
2024-07-31 12:33:13 +0200

    Update RL_agent.py
    modification pour structuration

--- RL_agent.py
+++ RL_agent.py
@@ -46,35 +46,4 @@ class StockNetwork(nn.Module):
         
         return action_sampled, bell, log_prob
 
-    @staticmethod
-    def simulate_price_heston(S0, V0, mu, kappa, theta, sigma, rho, days):
-        dt = 1 / days
-        prices = [S0]
-        volatilities = [V0]
-        S = S0
-        V = V0
-        for _ in range(days):
-            dW_S = np.random.normal(0, np.sqrt(dt))
-            dW_V = rho * dW_S + np.sqrt(1 - rho**2) * np.random.normal(0, np.sqrt(dt))
-            V = np.maximum(V + kappa * (theta - V) * dt + sigma * np.sqrt(V) * dW_V, 0)  
-            S = S * np.exp((mu - 0.5 * V) * dt + np.sqrt(V * dt) * dW_S)
-            prices.append(S)
-            volatilities.append(V)
-        return prices, volatilities
 
-    @staticmethod
-    def simulate_price(S_n, X, sigma):
-        prices = [S_n]
-        for x in X:
-            S_n = S_n + sigma * x
-            prices.append(S_n)
-        return prices
-    
-    @staticmethod
-    def normalize_state(state, days, goal, S0):
-        t, S_n, A_n, total_stocks, total_spent = state
-        return np.array([t / days, S_n / 100, A_n / 100, total_stocks / goal, total_spent / (goal * S0)])
-
-    @staticmethod
-    def payoff(A_n, total_spent):
-        return 100 * A_n - total_spent
