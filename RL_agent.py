276c20bc5aca5f641e6219c0dd0c891a357e3aca
Mark-Killian <mark-killian.zinenberg@edu.devinci.fr>
2024-08-01 00:12:56 +0200

    Update
    écriture à la main d'une loi de bernouilli ainsi que la log_density et vérification que la moyenne à un gradient

--- RL_agent.py
+++ RL_agent.py
@@ -28,6 +28,7 @@ class StockNetwork(nn.Module):
         x = self.act3(self.hidden3(x))
         mean = self.mean_output(x)
         bell = self.act_output(self.bell_output(x))
+        mean.retain_grad() # Ensure that the mean has a gradient
         
         return mean, bell
 
@@ -40,10 +41,19 @@ class StockNetwork(nn.Module):
         std = (goal / days) * 0.05 # fixed standard deviation to add some randomness to the actions
         dist = torch.distributions.Normal(mean, std)
         action_sampled = dist.sample() 
-        bell = torch.bernoulli(bell) # u loi uniforme de (0,1), si u < p alors bell = 1 sinon bell = 0
-        log_prob = dist.log_prob(action_sampled) # a la main
-        # 1/sqrt(2pi)*(1/std)*exp(-0.5*((x-mean)/std)^2)  vérifier que la moyenne à un gradient
+        u = np.random.uniform(0,1)
+        if u < bell:
+            bell = 1
+        else:
+            bell = 0
+
+        log_density = -0.5*np.log(2*np.pi*std**2)-((action_sampled-mean)**2)/(2*std**2)
+
+        prob = dist.cdf(action_sampled)
+
+
+        return action_sampled, bell, log_density, prob
+    
         
-        return action_sampled, bell, log_prob
 
 
